07ab7eb24e9664963bb38ce31d8e528c120a87a8
Ben Thompson <bathompson@win.cis.ksu.edu>
2019-11-19 15:09:27 -0600

    added classwork

--- Movies/MovieDatabase.cs
+++ Movies/MovieDatabase.cs
@@ -25,7 +25,54 @@ namespace Movies
                 movies = JsonConvert.DeserializeObject<List<Movie>>(json);
             }
         }
+        public List<Movie> Search(string search)
+        {
+            List<Movie> results = new List<Movie>();
+            foreach(Movie m in movies)
+            {
+                if (m.Title.Contains(search, StringComparison.OrdinalIgnoreCase) || m.Director!=null && m.Director.Contains(search, StringComparison.OrdinalIgnoreCase))
+                {
+                    results.Add(m);
+                }
+            }
+            return results;
+        }
 
+       public List<Movie> FilterByMPAA(List<string> mpaa)
+        {
+            List<Movie> r = new List<Movie>();
+            foreach(Movie m in movies)
+            {
+                if (mpaa.Contains(m.MPAA_Rating))
+                    r.Add(m);
+            }
+            return r;
+        }
+        public List<Movie> FilterByMPAAAndSearch(List<string> mpaa, string search)
+        {
+            List<Movie> r = new List<Movie>();
+            foreach (Movie m in movies)
+            {
+                if ((m.Title.Contains(search, StringComparison.OrdinalIgnoreCase) || m.Director != null && m.Director.Contains(search, StringComparison.OrdinalIgnoreCase))&& mpaa.Contains(m.MPAA_Rating))
+                {
+                    r.Add(m);
+                }
+            }
+            return r;
+        }
+
+        public List<Movie> FilterIMDB(float min, List<Movie> m)
+        {
+            List<Movie> r = new List<Movie>();
+
+            foreach(Movie l in m)
+            {
+                if (l.IMDB_Rating != null && l.IMDB_Rating >= min)
+                    r.Add(l);
+                
+            }
+            return r;
+        }
         public List<Movie> All { get { return movies; } }
     }
 }
--- Movies/Pages/Index.cshtml.cs
+++ Movies/Pages/Index.cshtml.cs
@@ -10,9 +10,35 @@ namespace Movies.Pages
     public class IndexModel : PageModel
     {
 
+        public MovieDatabase movieDatabase = new MovieDatabase();
+        public List<Movie> movies;
+
+        [BindProperty]
+        public string search { get; set; }
+
+        [BindProperty]
+        public List<string> mpaa { get; set; } = new List<string>();
+
+        [BindProperty]
+        public float minIMDB { get; set;  }
         public void OnGet()
         {
-
+            movies = movieDatabase.All;
+        }
+        public void OnPost()
+        {
+            if (search != null && mpaa.Count > 0)
+            {
+                movies = movieDatabase.FilterByMPAAAndSearch(mpaa, search);
+            }
+            else if (search != null)
+                movies = movieDatabase.Search(search);
+            else if (mpaa.Count > 0)
+                movies = movieDatabase.FilterByMPAA(mpaa);
+            if (minIMDB is float min)
+                movies = movieDatabase.FilterIMDB(min, movies);
+            else
+                movies = movieDatabase.All;
         }
     }
 }
